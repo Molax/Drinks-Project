b936b54696bd9a0e054da439c3cbfe0ceaf349e5
João Paulo Amorim Rosa <joaopaulo18@live.it>
2015-11-13 18:27:02 -0200

    DTO
    Created all the Data Table Objects in the application

---
+++ DrinksProject.Dto/Size.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksProject.Dto
+{
+    public class Size
+    {
+        public int PK_SIZE { get; set; }
+
+        public string SIZETYPE { get; set; }
+    }
+}
---
+++ DrinksProject.Dto/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksProject.Dto
+{
+    public class Type
+    {
+        public int PK_TYPE { get; set; }
+
+        public string NAME { get; set; }
+    }
+}
