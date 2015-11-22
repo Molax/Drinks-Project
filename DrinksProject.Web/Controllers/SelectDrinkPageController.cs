42b7ea2ace1813c0f008c75486f6f2592e1dc4cd
João Paulo Amorim Rosa <joaopaulo18@live.it>
2015-11-22 21:17:53 -0200

    New Page
    Page to select drinks

---
+++ DrinksProject.Web/Controllers/SelectDrinkPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppOnTime.Web.Controllers
+{
+    public class SelectDrinkPageController : Controller
+    {
+        //
+        // GET: /SelectDrinkPage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
